dd227a7cffb5eb1636c2b8fb1e8b6bf63d6c2259
eortiz16 <eortiz16@csub.edu>
2018-10-03 17:13:56 -0700

    Kgram minor updates

--- src/cecs429/query/WildCardLiteral.java
+++ src/cecs429/query/WildCardLiteral.java
@@ -4,7 +4,6 @@ import cecs429.index.Index;
 import cecs429.index.Posting;
 import cecs429.text.TokenProcessor;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 //Gererate 1, 2, and 3-grams for each vocabulary type in the vocabulary
@@ -13,34 +12,46 @@ import java.util.List;
 public class WildCardLiteral implements QueryComponent{
     
     // The list of individual terms in the phrase.
-    private List<String> mTerms = new ArrayList<>();
+    private KGram kGram = null;
 
-    public WildCardLiteral(List<String> terms) {
-            mTerms.addAll(terms);
-    }
-
-    public WildCardLiteral(String terms) {
-        new KGram(terms);
-            mTerms.addAll(Arrays.asList(terms.split(" ")));
+    public WildCardLiteral(String term) {
+        kGram = new KGram(term);
     }
 
     @Override
     public List<Posting> getPostings(Index index, TokenProcessor processor) {
         List<Posting> results = new ArrayList<>();
         
-        //generate the largest k-grams it can for its literal 
+        // generate the largest k-grams it can for its literal 
+        // kGram.getParsed() will retain largest kGram
+        String lrgKGram = getLargestKGram();
+        
         //retrieve and intersect the list of vocabulary  types for each k-gram from the k-gram index
         //Note: Wildcard index must have access to kgram index
+        
         //the intersected list will contain all candidate type strings
         //implement a post filtering step to ensure that each candidate matches the wildcard pattern
-        //OR together the postings fir the processed term from each final wildcard candiate and return them as the postings list for wild cardLiteral
+        //OR together the postings for the processed term from each final wildcard candiate and return them as the postings list for wild cardLiteral
         
         return results;
     }
 
+    private String getLargestKGram() {
+        int largestLength = 0;
+        int indexLarge = 0;
+        
+        for (int i = 0; i < kGram.getParsedTerm().size(); ++i) {
+            if (kGram.getParsedTerm().get(i).length() > largestLength) {
+                largestLength = kGram.getParsedTerm().get(i).length();
+                indexLarge = i; 
+            }
+        }
+        
+        return kGram.getParsedTerm().get(indexLarge);
+    }
 
     @Override
     public String toString() {
-        return "\"" + String.join(" ", mTerms) + "\"";
+        return kGram.getmTerm();
     }
 }
